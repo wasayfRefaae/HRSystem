b61c427c882d8d66ed655f754318b6058d4b2ce4
wasayfRefaae <wasaief.refaae@gmail.com>
2025-12-25 15:43:36 +0300

    fiveCommit

--- app/Filament/Resources/Users/RelationManagers/IncidentRequestRelationManager.php
+++ app/Filament/Resources/Users/RelationManagers/IncidentRequestRelationManager.php
@@ -11,7 +11,7 @@ class IncidentRequestRelationManager extends RelationManager
 {
     protected static string $relationship = 'incidentRequest';
 
-    protected static ?string $relatedResource = IncidentRequestResource::class;
+  //  protected static ?string $relatedResource = IncidentRequestResource::class;
 
     public function table(Table $table): Table
     {
